d3c00d051fcdc7b3ece1c338c059ac0ec869682f
hariram77142006-bit <hariram77142006@gmail.com>
2026-07-20 02:22:10 -0700

    Create largestarray.java

---
+++ largestarray.java
@@ -0,0 +1,17 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class Main {
+    public static void main(String[] args) {
+    int []arr={30,69,99,11,33};
+    int max=arr[0];
+    for (int i=0;i<arr. length;i++)
+    {
+        if(arr[i]>max)
+    {
+        max=arr[i];
+    }
+    }
+    System.out.println("maximum element;"+max);
+    }
+}
