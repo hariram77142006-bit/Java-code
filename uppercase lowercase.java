8d568ab21199dcdfd0268cec247e3caabdd1e15b
hariram77142006-bit <hariram77142006@gmail.com>
2026-07-22 08:33:11 -0400

    Create uppercase lowercase.java

---
+++ uppercase lowercase.java
@@ -0,0 +1,8 @@
+mport java.util.*;
+public class main{
+    public static void main (String[]args){
+    String name="java";
+    System.out.println(name.toUpperCase());
+    System.out.println(name.toLowerCase());
+    }
+    }
