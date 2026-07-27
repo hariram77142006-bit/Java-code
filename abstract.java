6f7bb5e3f2eb4f7736875a1b21bc30cca4b7f1ef
hariram77142006-bit <hariram77142006@gmail.com>
2026-07-27 14:32:23 +0530

    Add abstract class Animal and Dog implementation

---
+++ abstract.java
@@ -0,0 +1,19 @@
+
+ abstract class Animal{
+    abstract void sound();
+void sleep(){    
+    System.out.println("animal is sleeping");
+    }
+ }
+    class Dog extends Animal{
+        void sound(){
+         System.out.println("Dog barks");    
+        }
+    }
+    public class Main{
+        public static void main(String[] args){
+            Dog d=new Dog();
+            d.sound();
+            d.sleep();
+        }
+    }
